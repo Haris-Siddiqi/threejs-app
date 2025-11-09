ab61be75d9316ab1350bf2d9c9f4522e311353e8
Haris Siddiqi <harisu2550@gmail.com>
2025-11-08 19:59:33 -0500

    Macbook model

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,8 +1,10 @@
-import React, { use } from 'react'
+import React from 'react'
 import useMacbookStore from '../store'
 import clsx from 'clsx';
 import {Canvas} from '@react-three/fiber';
 import { Box, OrbitControls } from '@react-three/drei';
+import MacbookModel14 from './models/Macbook-14.jsx';
+import StudioLights from './StudioLights.jsx';
 
 
 const ProductViewer = () => {
@@ -45,7 +47,8 @@ const ProductViewer = () => {
         </div>
 
         <Canvas id="canvas" camera={{ position: [0, 0, 5], fov: 50, near: 0.1, far:100}}>
-            <Box position={[0, 0, 0]} scale={10 * scale} material-color={color} />
+            <StudioLights />
+            <MacbookModel14 scale={0.06} position={[0, 0, 0]} />    
             <OrbitControls enableZoom={false} />
         </Canvas>
     </section>
